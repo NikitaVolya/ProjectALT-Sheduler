b629cd4f1bcac2768b547dfa723ebcdf9c958b7b
NikitaVolia <volanskijnikita2@gmail.com>
2026-02-03 08:39:04 +0100

    Add MariaDB

--- src/main.c
+++ src/main.c
@@ -1,99 +1,89 @@
-#include <SDL2/SDL.h>
+#include <mariadb/mysql.h>
+#include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
-#define WINDOW_TITLE "Test APP"
-#define SCREEN_WIDTH 800
-#define SCREEN_HEIGHT 600
 
-struct App {
-    SDL_Window *window;
-    SDL_Renderer *renderer;
-    int exit;
-};
+void read_table_test(MYSQL *conn) {
+    MYSQL_STMT *stmt;
+    MYSQL_BIND bind[1];
+    char name[256];
+    unsigned long name_length;
+    my_bool is_null;
 
-int sdl_initialize(struct App *app) {
-    int rep = SDL_Init(SDL_INIT_EVERYTHING);
-    
-    if (rep) {
-        fprintf(stderr, "Error initializing SDL: %s\n", SDL_GetError());
-    } else {
-        app->window = SDL_CreateWindow(WINDOW_TITLE,
-                         SDL_WINDOWPOS_CENTERED,
-                         SDL_WINDOWPOS_CENTERED,
-                         SCREEN_WIDTH,
-                         SCREEN_HEIGHT,
-                         0);
-
-    }
-    if (!rep && !app->window) {
-        fprintf(stderr, "Error creating window: %s\n", SDL_GetError());
-        rep = 1;
-    } else {
-        app->renderer = SDL_CreateRenderer(app->window, -1, 0);
-    }
-
-    if (!rep && !app->renderer) {
-        fprintf(stderr, "Error creating renderer: %s\n", SDL_GetError());
-        rep = 1;
+    if ((stmt = mysql_stmt_init(conn)) == NULL) {
+        fprintf(stderr, "Error while initialize mysql stmt\n");
+        return;
     }
     
-    return rep;
-}
+    if (mysql_stmt_prepare(stmt, "SELECT name FROM test;", 23)) {
+        fprintf(stderr, "Error while preparing query\n");
+        mysql_stmt_close(stmt);
+        return;
+    }
 
-void app_cleanup(struct App *app) {
-    SDL_DestroyRenderer(app->renderer);
-    SDL_DestroyWindow(app->window);
-    SDL_Quit();
+    if (mysql_stmt_execute(stmt)) {
+        fprintf(stderr, "Error while executing query\n");
+        mysql_stmt_close(stmt);
+        return;
+    }
+    memset(bind, 0, sizeof(bind));
+    bind[0].buffer_type   = MYSQL_TYPE_STRING;
+    bind[0].buffer        = name;
+    bind[0].buffer_length = sizeof(name);
+    bind[0].length        = &name_length;
+    bind[0].is_null       = &is_null;
+
+    if (mysql_stmt_bind_result(stmt, bind)) {
+        fprintf(stderr, "Bind result failed: %s\n", mysql_stmt_error(stmt));
+        mysql_stmt_close(stmt);
+        return;
+    }
+
+    if (mysql_stmt_store_result(stmt)) {
+        fprintf(stderr, "Store result failed: %s\n", mysql_stmt_error(stmt));
+        mysql_stmt_close(stmt);
+        return;
+    }
+
+    while (mysql_stmt_fetch(stmt) == 0) {
+        name[name_length] = '\0';
+        printf("Name: %s\n", name);
+    }
+
+    mysql_stmt_free_result(stmt);
+    mysql_stmt_close(stmt);
 }
 
 int main() {
-    SDL_Event event;
-    
-    struct App app;
-    app.window = NULL;
-    app.renderer = NULL;
-    app.exit = 0;
+    MYSQL *conn;
 
-    if (sdl_initialize(&app)) {
-        app_cleanup(&app);
-        exit(EXIT_FAILURE);
+
+    
+    if (!(conn = mysql_init(0))) {
+        fprintf(stderr, "unable to initialize connection struct\n");
+        exit(1);
     }
 
-    while (!app.exit) {
-
-        while (SDL_PollEvent(&event)) {
-            switch (event.type) {
-            case SDL_QUIT:
-                app.exit = 1;
-                break;
-            case SDL_KEYDOWN:
-                switch(event.key.keysym.scancode) {
-                case SDL_SCANCODE_ESCAPE:
-                    app.exit = 1;
-                    break;
-                case SDL_SCANCODE_SPACE:
-                    SDL_SetRenderDrawColor(app.renderer, 255, 255, 255, 255);
-                    break;
-                default:
-                    break;
-                }
-                break;
-            default:
-                break;
-            }
-        }
-        
-
-        SDL_RenderClear(app.renderer);
-        SDL_RenderPresent(app.renderer);
-
-        SDL_Delay(10);
-
+    if (!mysql_real_connect(
+        conn,
+        "127.0.0.1",
+        "app_user",
+        "strong_password",
+        "test",
+        3306,
+        NULL,
+        0
+    )) {
+        printf("Connection failed: %s\n", mysql_error(conn));
+        return 1;
     }
     
-    app_cleanup(&app);
+    printf("Connected succesfully!\n");
 
-    printf("App end\n");
+    read_table_test(conn);
+    
+    mysql_close(conn);
     
     exit(EXIT_SUCCESS);
 }
