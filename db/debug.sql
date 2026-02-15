3e62af0868cf515604f7e39228ac55bedab3d678
NikitaVolia <volanskijnikita2@gmail.com>
2026-02-15 18:18:55 +0100

    Add p_generate_day_work_time

--- db/debug.sql
+++ db/debug.sql
@@ -5,16 +5,29 @@ source procedures.sql
 source function.sql
 source triggers.sql
 
-INSERT INTO worker(first_name, second_name) VALUES ('test', 'worker');
-INSERT INTO worker(first_name, second_name) VALUES ('second', 'worker');
+INSERT INTO worker(first_name, second_name)
+VALUES ('test', 'worker'),
+       ('second', 'worker')
+;
 
-INSERT INTO role(name) VALUES ('test');
+INSERT INTO role(name)
+VALUES ('test')
+;
 
-INSERT INTO worker_role VALUES (1, 1);
+INSERT INTO worker_role(worker_id, role_id)
+VALUES (1, 1),
+       (2, 1)
+;
 
-INSERT INTO line(name, product) VALUES ('A1', 'toy'), ('B1', 'toy');
+INSERT INTO line(name, product)
+VALUES ('A1', 'toy'),
+       ('B1', 'toy')
+;
 
-INSERT INTO line_role VALUES (1, 1, 1), (2, 1, 1);
+INSERT INTO line_role(line_id, role_id, count)
+VALUES (1, 1, 1),
+       (2, 1, 1)
+;
 
 CALL p_create_line_work_default_day(6, 1);
 CALL p_insert_work_time(1, "10:00:00", "12:00:00");
@@ -31,10 +44,10 @@ CALL p_insert_work_time(4, "15:30:00", "20:00:00");
 
 
 
-DROP PROCEDURE IF EXISTS p_generate_work_time;
+DROP PROCEDURE IF EXISTS p_generate_line_work_time;
 
 DELIMITER $
-CREATE PROCEDURE p_generate_work_time(
+CREATE PROCEDURE p_generate_line_work_time(
        IN in_date DATE,
        IN in_id_line INT UNSIGNED,
        IN in_work_time INT UNSIGNED
@@ -42,6 +55,7 @@ CREATE PROCEDURE p_generate_work_time(
 BEGIN
         DECLARE v_work_week_id INT UNSIGNED;
         DECLARE v_work_default_day_id INT UNSIGNED;
+        DECLARE v_line_work_week_day_id INT UNSIGNED;
         DECLARE v_start_time TIME;
         DECLARE v_end_time TIME;
         
@@ -82,6 +96,40 @@ BEGIN
                 AND lwdd.work_default_day_id = wt.work_day_id
                 AND wt.id = in_work_time
                 ;
+        END;
+
+        /* checking on existing line_work_week_day */
+        /* if exists in this time raise error */
+        /* else if exists but not in this time */
+        /* else insert new line_work_week_day */
+        BEGIN
+                /* creating if not exists */
+                DECLARE CONTINUE HANDLER
+                FOR NOT FOUND
+                BEGIN
+                        CALL p_create_line_work_week_day(v_work_week_id, in_date, in_id_line);
+                        SET v_line_work_week_day_id = LAST_INSERT_ID();
+                END;
+                
+
+                SELECT lwwd.work_week_day_id
+                INTO v_line_work_week_day_id
+                FROM line_work_week_day AS lwwd,
+                     work_week_day AS wwd
+                WHERE lwwd.work_week_day_id = wwd.work_day_id
+                AND lwwd.line_id = in_id_line
+                AND wwd.date = in_date
+                ;
+
+                IF (SELECT COUNT(id)
+                    FROM work_time
+                    WHERE work_day_id = v_line_work_week_day_id
+                    AND (start_time BETWEEN v_start_time AND v_end_time)
+                    AND (end_time BETWEEN v_start_time AND v_end_time)) <> 0 THEN
+                    SIGNAL SQLSTATE '45000'
+                    SET MESSAGE_TEXT = 'line is already work in this time'
+                    ;
+                END IF;
         END;
 
         /* table of workers and time when they can work */
@@ -214,7 +262,10 @@ BEGIN
                         END;
 
                         /* inserting work time */
+                        /* for worker */
                         CALL p_insert_work_time(v_worker_work_week_day_id, v_start_time, v_end_time);
+                        /* for line */
+                        CALL p_insert_work_time(v_line_work_week_day_id, v_start_time, v_end_time);
 
                 END LOOP l_workers_queue;
         END;
@@ -222,9 +273,80 @@ END;
 $
 DELIMITER ;
 
-CALL p_generate_work_time("2026-02-15", 1, 2);
+DROP PROCEDURE IF EXISTS p_generate_day_work_time;
+DELIMITER $
+CREATE PROCEDURE p_generate_day_work_time(
+       IN in_date DATE
+)
+BEGIN
+        DECLARE v_continue BOOLEAN DEFAULT TRUE;
+        DECLARE v_line_id INT UNSIGNED;
+        DECLARE v_work_time_id INT UNSIGNED;
+
+        DECLARE c_default_time CURSOR FOR
+        SELECT lwdd.line_id, wt.id
+        FROM line_work_default_day AS lwdd,
+             work_default_day AS wdd,
+             work_time AS wt
+        WHERE lwdd.work_default_day_id = wt.work_day_id
+        AND lwdd.work_default_day_id = wdd.work_day_id
+        AND wdd.day_number = WEEKDAY(in_date)
+        ORDER BY TIMEDIFF(wt.end_time, wt.start_time)
+        ;
+
+        DECLARE CONTINUE HANDLER
+        FOR NOT FOUND
+        SET v_continue = FALSE;
+
+        OPEN c_default_time;
+
+        l_default_work_days: LOOP
+                
+                FETCH c_default_time
+                INTO v_line_id, v_work_time_id
+                ;
+
+                IF v_continue = FALSE THEN
+                   LEAVE l_default_work_days;
+                END IF;
+
+                BEGIN
+
+                        DECLARE CONTINUE HANDLER
+                        FOR SQLEXCEPTION
+                        SELECT CONCAT('Imosible to generate work day for ',
+                                       CAST(in_date AS CHAR),
+                                       ' ',
+                                       (SELECT CONCAT(CAST(start_time AS CHAR),
+                                                      ' -> ' ,
+                                                      CAST(end_time AS CHAR))
+                                        FROM work_time
+                                        WHERE id = v_work_time_id
+                                        LIMIT 1),
+                                       ' and line ',
+                                       CAST(v_line_id AS CHAR),
+                                       ', because its already exists.') AS message
+                                       ;
+
+                        CALL p_generate_line_work_time(in_date, v_line_id, v_work_time_id);
+                END;
+                
+        END LOOP l_default_work_days;
+
+        CLOSE c_default_time;
+END;
+$
+DELIMITER ;
+
+CALL p_generate_day_work_time("2026-02-15");
 
 SELECT wwwd.work_week_day_id AS work_day_id, worker_id, line_id, start_time, end_time
 FROM worker_work_week_day AS wwwd, work_time AS wt
 WHERE wwwd.work_week_day_id = wt.work_day_id
+ORDER BY start_time
 ;
+
+SELECT *
+FROM line_work_week_day, work_time
+WHERE line_work_week_day.work_week_day_id = work_time.work_day_id
+ORDER BY start_time;
